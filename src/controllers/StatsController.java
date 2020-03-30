3357c3e5956313606f4ad8f79e02f7bd287e0fde
Alina <varkkia99@gmail.com>
2020-03-30 01:08:47 +0100

    top 10 almost done

--- src/controllers/StatsController.java
+++ src/controllers/StatsController.java
@@ -5,12 +5,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import player.Player;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 
 public class StatsController {
 
+    private static final String playerDirectory = "resources\\players\\";
+    HashMap<String,Integer> hiScore = new HashMap<>();
+
+
+    @FXML
+    private GridPane playersTop;
     @FXML
     private Label totalGuesses;
     @FXML
@@ -24,6 +34,9 @@ public class StatsController {
 
     @FXML
     private void initialize() {
+        addNumbers();
+        fillTop();
+        fillTopGrid();
         totalGuesses.setText("      Total guesses: " + LogInController.currentPlayer.totalGuesses);
         correctGuesses.setText("      Correct guesses: " + LogInController.currentPlayer.correctGuesses);
         DecimalFormat df = new DecimalFormat("#.##");
@@ -42,5 +55,33 @@ public class StatsController {
             e.printStackTrace();
         }
         cont.activate("menu");
+    }
+
+    //reading all players and adding their scores to the map
+    public void fillTop(){
+        File[] files = new File(playerDirectory).listFiles();
+        for (File file : files) {
+            try {
+                FileInputStream saveFile = new FileInputStream(file);
+                ObjectInputStream restore = new ObjectInputStream(saveFile);
+                Player user = (Player) restore.readObject();
+                hiScore.put(user.username, user.cryptogramsCompleted);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void fillTopGrid(){
+        
+    }
+
+    public void addNumbers(){
+        for(int i = 0; i < playersTop.getRowCount(); i++){
+            Text e = new Text();
+            e.setText(String.valueOf(i+1));
+            playersTop.add(e, 0, i);
+        }
     }
 }
